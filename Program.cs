07bf795211f3aaca45cf906218b14d953710c8f5
dersime <dersimepl@gmail.com>
2024-02-19 10:50:24 +0100

    added appsettings to hide confidential information

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AsyncAwaitBestPractices;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true);
+
+var config = builder.Build();
+
+var channel = config["channel"];
+var userName = config["user:username"];
+Console.WriteLine($"Channel is: {channel}");
+Console.WriteLine($"User name is: {userName}");
